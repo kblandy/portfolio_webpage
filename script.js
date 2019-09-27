f63f85e42a8f786502c963529030450aeb92b402
Kirstyn Blandy <kirstyn.blandy@gmail.com>
2019-09-27 12:36:42 -0400

    finished smooth scrolling on main page, added bootstrap and full jquery to project, will need to remove unneeded files before production

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var arrowButton = $(".arrow");
 var mainContent = $("#main-container");
 
+//adds gentle scrolling when arrow icon is clicked
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    arrowButton.on('click', function(event) {
 
-arrowButton.on("click", function(event){
-    alert("button clicked!")
-});
-
-
+        event.preventDefault();
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: mainContent.offset().top
+        }, 900, function(){
+     
+        });
+      }); // End if
+    });
